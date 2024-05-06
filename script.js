bc06829ddfadbf9bea2f10a175139baacf69e882
Juan Pablo <jpcorvo219@gmail.com>
2024-05-06 18:30:30 -0300

    Major update
    - Removed map style from HTML
    - Changed title in HTML
    - Added title icon in HTML
    - Created Json with data
    - Added marker functionality in JS
    - Added custom markers in JS
    - Added custom pin png
    - Added logo png

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 var map = L.map('map').setView([-34.547303588817826, -55.50233092972566], 9);
+var shaolinPin = L.icon({
+    iconUrl: './Img/Shaolin pin.png',
+
+    iconSize:     [65, 95],
+    iconAnchor:   [30, 94],
+    popupAnchor:  [2, -79]
+});
+
+
 
 L.tileLayer('https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=th1B7ltIffQzw1VZAQcG', {
     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
 }).addTo(map);
-var pinar = L.marker([-34.78717173604009, -55.89486317584626],).addTo(map).bindPopup("Mborayú - Espacio Biocéntrico <br> Profesor: Diego Correa <br><a href='https://maps.app.goo.gl/hefHT9xZ3zVFcFY27'>Ver en maps</a>");
-var solymar = L.marker([-34.82516548301583, -55.962945784969556],).addTo(map).bindPopup("Gimnasio Centro Alquimia <br> Profesor: Diego Correa <br><a href='https://maps.app.goo.gl/sEnEC5qPvzwsXrHM8'>Ver en maps</a>");
-var pinamar = L.marker([-34.78834722355707, -55.85262065324699],).addTo(map).bindPopup("El Club Pinamar <br> Profesor: Juanma de Tradicional y Moderno <br><a href='https://maps.app.goo.gl/y3HZoqdoFCfvsMVP7'>Ver en maps</a>");
-var atlantida = L.marker([-34.76703757055167, -55.763955990327986],).addTo(map).bindPopup("Shaolin Chuan <br> Profesor: Shifu Daniel Bogado <br><a href='https://maps.app.goo.gl/Za8q6SkZLaEq76zX6'>Ver en maps</a>");
 
+function getData() {
+    const response = fetch('./locales.json')
+        .then(response => response.json())
+        .then(data => createMarker(data))
+}
 
+function createMarker(data) {
+    data["locales"].forEach(object => {
+        L.marker([object.latitud, object.longitud],{icon: shaolinPin}).addTo(map).bindPopup(object.lugar + "<br>" + object.profesor + "<br><a href='https://api.whatsapp.com/send?phone=" + object.numero + "&text=Hola, quisiera averiguar sobre las clases de "+ object.disciplina +"'>Whatsapp</a>" +"<br><a href='"+ object.link + "'>Ver en maps</a>");
+    });
 
+}
 
-
-
-
-
-//var  = L.marker([],).addTo(map).bindPopup(" <br> Profesor:  <br><a href=''>Ver en maps</a>");
+document.addEventListener("DOMContentLoaded", () => {
+    getData()
+})
